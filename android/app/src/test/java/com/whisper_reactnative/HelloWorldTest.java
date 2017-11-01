dd34ae9e9c66c1f0319de27720270f8ebd29da20
Aren Chen <yarencheng@gmail.com>
2017-11-01 21:27:24 +0800

    hello android unit test

---
+++ android/app/src/test/java/com/whisper_reactnative/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.whisper_reactnative;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class HelloWorldTest {
+
+    @Test
+    public void echo() {
+        System.out.println("hello test");
+        assertTrue(true);
+        // assertTrue(false);
+    }
+}
